5f29f64a44025e24e107a3c8357aeb8ab6b41b0a
42final <skuntoji@e1z2r5p3.42.us.org>
2018-09-13 12:56:42 -0700

    ft_countdown

---
+++ 0-0-ft_countdown/README.md
@@ -0,0 +1,12 @@
+Assignment name  : ft_countdown <br>
+Expected files   : ft_countdown.c ,br>
+Allowed functions: write<br>
+--------------------------------------------------------------------------------<br>
+
+Write a program that displays all digits in descending order, followed by a
+newline.<br>
+
+Example:<br>
+$> ./ft_countdown | cat -e<br>
+9876543210$<br>
+$><br>
--- 0-0-ft_countdown/ft_countdown_loop.c
+++ 0-0-ft_countdown/ft_countdown_loop.c
@@ -1,36 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   aff_a.c                                            :+:      :+:    :+:   */
+/*   ft_countdown_loop.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skuntoji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/09/13 12:27:06 by skuntoji          #+#    #+#             */
-/*   Updated: 2018/09/13 12:46:47 by skuntoji         ###   ########.fr       */
+/*   Created: 2018/09/13 12:53:34 by skuntoji          #+#    #+#             */
+/*   Updated: 2018/09/13 12:55:49 by skuntoji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int 	main(int ac, char **av)
+int	main(void)
 {
-	int i;
+	char i;
 
-	i = 0;
-	if (ac == 2)
+	i = '9';
+	while (i >= '0')
 	{
-		while (av[1][i])
-		{
-			if (av[1][i] == 'a')
-			{
-				write(1,"a\n",2);
-				return (0);
-			}
-			i++;
-		}
-		write(1,"\n",1);
-		return (0);
+		write(1,&i,1);
+		i--;
 	}
-	write (1, "a\n",2);
+	write(1, "\n", 1);
 	return (0);
 }
